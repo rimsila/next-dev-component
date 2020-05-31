b82d729e1836fb7fae0adb6729a7d5c1b170356f
rimsila.itc <rimsila.itc@gmail.com>
2020-06-01 00:24:33 +0700

    updated config and remove unused dp

--- src/components/NextCard/index.tsx
+++ src/components/NextCard/index.tsx
@@ -1,13 +1,14 @@
 // Generated with util/create-component.js
-import React from "react";
+import React from 'react';
 
-import { NextCardProps } from "./NextCard.types";
+import { NextCardProps } from './NextCard.types';
 
-import "./NextCard.scss";
+import './NextCard.scss';
 
 const NextCard: React.FC<NextCardProps> = ({ foo }) => (
-    <div data-testid="NextCard" className="foo-bar">{foo}</div>
+  <div data-testid="NextCard" className="foo-bar">
+    {foo}
+  </div>
 );
 
 export default NextCard;
-
